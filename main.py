bed1f702957a301ba97fc51a53fac6b9e6eb6419
CSMore <erickaro98@gmail.com>
2024-04-21 02:03:24 -0600

    Added joblib andthe model

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import JSONResponse
+import pandas as pd
+import joblib
+
+
+app = FastAPI(
+    title="Deploy Social Network Adds",
+    version="0.0.1"
+)
+
+# ------------------------------------------------------------
+# LOAD THE AI MODEL
+# ------------------------------------------------------------
+model = joblib.load("model/linear_regression_model_v01.pkl")
+
+
+@app.post(path="/api/v1/predict-social-network-adds", tags=["social-network-adds"])
+async def predict(
+    Gender: float,
+    Age: float,
+    EstimatedSalary
+):
+    dictionary = {
+        "Gender": Gender,
+        "Age": Age,
+        "EstimatedSalary": EstimatedSalary
+    }
+
+
+    try:
+        df = pd.DataFrame(dictionary, index=[0])
+        # prediction = model.predict(df)
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=1
+        )
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e),
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
